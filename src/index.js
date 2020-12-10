b40c9cf69b6d64654f72b3230da939dbbfc21537
laurelisthompson <laurelis.thompson@gmail.com>
2020-12-10 10:27:45 -0500

    progress with d3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles/index.scss";
+import * as d3 from 'd3';
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("testing");
+    // console.log(d3.hierarchy());
+
+})
+
---
+++ src/scripts/map.js
@@ -0,0 +1,9 @@
+import * as d3 from 'd3';
+
+
+export default graphicMap => {
+
+    const svg = d3.create("svg")
+        .attr("viewBox", [0, 0, width, height]);
+}
+
