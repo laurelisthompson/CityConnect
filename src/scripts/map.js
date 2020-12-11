c28317493dc2a58326ef24fb2e591c950b70a925
laurelisthompson <laurelis.thompson@gmail.com>
2020-12-11 15:40:23 -0500

    add map consts and functions

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -3,75 +3,81 @@ import dataObj from './data'
 
 export default graphicMap => {
 
-    //define constants
-    const root = pack(dataObj);
-    let focus = root; //keep track of which circle the chart is focused on
-    let view;
+	//define constants
+	const root = pack(dataObj);
+	let focus = root; //keep track of which circle the chart is focused on
+	let view;
 
-    const svg = d3.create("svg");
-    svg.attr("viewBox", [0, 0, width, height]);
-    //add additional styling
-    svg.on("click", (event) => zoom(event, root));
+	const svg = d3.create("svg");
+	svg.attr("viewBox", [0, 0, width, height]);
+	//add additional styling
+	svg.on("click", (event) => zoom(event, root));
 
-    const node = svg.append("g");
-    node.selectAll("circle");
-    node.data(root.descendants().slice(1)); //get all node children, exclude itself
-    node.join("circle");
-    //add additional styling
-    node.on("click", (event, node) => {
-        if (focus !== node) {
-            zoom(event, node);
-            event.stopPropogation();
-        };
-    });
+	const node = svg.append("g");
+	node.selectAll("circle");
+	node.data(root.descendants().slice(1)); //get all node children, exclude itself
+	node.join("circle");
+	//add additional styling
+	node.on("click", (event, node) => {
+		if (focus !== node) {
+			zoom(event, node);
+			event.stopPropogation();
+		};
+	});
 
-    const textLabel = svg.append("g");
-    textLabel.selectAll("text");
-    textLabel.data(root.descendants());
-    textLabel.join("text");
+	const textLabel = svg.append("g");
+	textLabel.selectAll("text");
+	textLabel.data(root.descendants());
+	textLabel.join("text");
 
 
-    //define functions
-    zoom = (event, node) => {
-        const oldFocus = focus;
-        focus = node;
 
-        //create zoom transition constant, set duration and call tween
-        const zoomTransition = svg.transition();
-        zoomTransition.duration(750);
-        zoomTransition.tween("zoom", node => {
-            //create interpolator for the two views
-            const interpolator = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-            
-            //t is the % of duration that has elapsed since the click
-            return t => zoomTo(interpolator(t));
-        });
-    };
+	//define functions
+	zoomTo([root.x, root.y, root.r * 2]);
+	zoomTo = (v) => { //takes in a view 
+		//find proportion of the width to the diameter
+		const prop = width / v[2]
+		view = v
+		
+		//transform/translate labels and nodes
+		let xDif = node.x - v[0];
+		let yDif = node.y - v[1];
+		node.attr("transform", node => `translate(${xDif * prop}, ${yDif * prop})`);
+		textLabel.attr("transform", node => `translate(${xDif * prop}, ${yDif * prop})`);
+		//update nodes radius
+		node.attr("r", node => {
+			node.r * prop;
+		});
+	};
+	
+	zoom = (event, node) => {
+		const oldFocus = focus;
+		focus = node;
 
-    zoomTo([root.x, root.y, root.r * 2]);
-
-    zoomTo = (v) => { //takes in a view 
-        //find proportion of the width to the diameter
-        const proportion = width / v[2]
-        view = v
-
-        //transform/translate labels and nodes
-        //update nodes radius
-    }
+		//create zoom transition constant, set duration and call tween
+		const zoomTransition = svg.transition();
+		zoomTransition.duration(750);
+		zoomTransition.tween("zoom", node => {
+			//create interpolator for the two views
+			//t is the % of duration that has elapsed since the click
+			const interpolator = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+			return t => zoomTo(interpolator(t));
+		});
+	};
 };
 
 pack = (dataObj) => {
-    //creates new pack layout and sets size and padding values
-    const packInstance = d3.pack();
-    packInstance.size([width, height]);
-    packInstance.padding(5);
+	//creates new pack layout and sets size and padding values
+	const packInstance = d3.pack();
+	packInstance.size([width, height]);
+	packInstance.padding(5);
 
-    //create root node by passing into d3 hierarchy, calculating value and sorting by nodes values
-    const rootNode = d3.heirarchy(dataObj); 
-    rootNode.sum(d => d.value);
-    rootNode.sort((a, b) => b.value - a.value);
+	//create root node by passing into d3 hierarchy, calculating value and sorting by nodes values
+	const rootNode = d3.heirarchy(dataObj); 
+	rootNode.sum(d => d.value);
+	rootNode.sort((a, b) => b.value - a.value);
 
-    packInstance(rootNode);
+	packInstance(rootNode);
 };
 
 const width = 900;
