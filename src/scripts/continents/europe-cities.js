4f0a6a0c117ad8af527bdb10261e5f148ef5b145
laurelisthompson <laurelis.thompson@gmail.com>
2021-02-05 17:53:30 -0500

    add europe cities

--- src/scripts/continents/europe-cities.js
+++ src/scripts/continents/europe-cities.js
@@ -4,12 +4,287 @@
 
 export const europe = [
     {
-        name: "",
+        name: "Russia",
         children: [
             {
-                name: "",
-                population: 0,
-                ranking: 0,
+                name: "Moscow",
+                population: 10.381222,
+                ranking: 1,
+            },
+            {
+                name: "Saint Petersburg",
+                population: 5.028,
+                ranking: 3,
+            },
+            {
+                name: "Novosibirsk",
+                population: 1.419007,
+                ranking: 18,
+            },
+            {
+                name: "Yekaterinburg",
+                population: 1.349772,
+                ranking: 19,
+            },
+            {
+                name: "Nizhniy Novgorod",
+                population: 1.284164,
+                ranking: 20,
+            },
+            {
+                name: "Samara",
+                population: 1.13473,
+                ranking: 27,
+            },
+            {
+                name: "Omsk",
+                population: 1.129281,
+                ranking: 28,
+            },
+            {
+                name: "Kazan",
+                population: 1.104738,
+                ranking: 29,
+            },
+            {
+                name: "Rostov",
+                population: 1.074482,
+                ranking: 30,
+            },
+            {
+                name: "Chelyabinsk",
+                population: 1.062919,
+                ranking: 31,
+            },
+            {
+                name: "Ufa",
+                population: 1.033338,
+                ranking: 32,
+            },
+            {
+                name: "Dnipro",
+                population: 1.032822,
+                ranking: 33,
+            },
+            {
+                name: "Volgograd",
+                population: 1.011417,
+                ranking: 37,
+            },
+        ]
+    },
+    {
+        name: "United Kingdom",
+        children: [
+            {
+                name: "London",
+                population: 7.5569,
+                ranking: 2,
+            },
+        ]
+    },
+    {
+        name: "Germany",
+        children: [
+            {
+                name: "Berlin",
+                population: 3.426354,
+                ranking: 4,
+            },
+            {
+                name: "Hamburg",
+                population: 1.739117,
+                ranking: 12,
+            },
+            {
+                name: "Munich",
+                population: 1.260391,
+                ranking: 22,
+            },
+        ]
+    },
+    {
+        name: "Spain",
+        children: [
+            {
+                name: "Madrid",
+                population: 3.255944,
+                ranking: 5,
+            },
+            {
+                name: "Barcelona",
+                population: 1.621537,
+                ranking: 15,
+            },
+        ]
+    },
+    {
+        name: "Ukraine",
+        children: [
+            {
+                name: "Kyiv",
+                population: 2.797553,
+                ranking: 6,
+            },
+            {
+                name: "Kharkiv",
+                population: 1.430885,
+                ranking: 17,
+            },
+            {
+                name: "Donetsk",
+                population: 1.0247,
+                ranking: 34,
+            },
+            {
+                name: "Odessa",
+                population: 1.001558,
+                ranking: 38,
+            },
+        ]
+    },
+    {
+        name: "Italy",
+        children: [
+            {
+                name: "Rome",
+                population: 2.318895,
+                ranking: 7,
+            },
+            {
+                name: "Milan",
+                population: 1.236837,
+                ranking: 23,
+            },
+        ]
+    },
+    {
+        name: "France",
+        children: [
+            {
+                name: "Paris",
+                population: 2.138551,
+                ranking: 8,
+            },
+        ]
+    },
+    {
+        name: "Romania",
+        children: [
+            {
+                name: "Bucharest",
+                population: 1.877155,
+                ranking: 9,
+            },
+        ]
+    },
+    {
+        name: "Belarus",
+        children: [
+            {
+                name: "Minsk",
+                population: 1.742124,
+                ranking: 10,
+            },
+        ]
+    },
+    {
+        name: "Hungary",
+        children: [
+            {
+                name: "Budapest",
+                population: 1.741041,
+                ranking: 11,
+            },
+        ]
+    },
+    {
+        name: "Poland",
+        children: [
+            {
+                name: "Warsaw",
+                population: 1.703139,
+                ranking: 13,
+            },
+        ]
+    },
+    {
+        name: "Austria",
+        children: [
+            {
+                name: "Vienna",
+                population: 1.691468,
+                ranking: 14,
+            },
+        ]
+    },
+    {
+        name: "Sweden",
+        children: [
+            {
+                name: "Stockholm",
+                population: 1.515017,
+                ranking: 16,
+            },
+        ]
+    },
+    {
+        name: "Serbia",
+        children: [
+            {
+                name: "Belgrade",
+                population: 1.273651,
+                ranking: 21,
+            },
+        ]
+    },
+    {
+        name: "Czech Republic",
+        children: [
+            {
+                name: "Prague",
+                population: 1.165581,
+                ranking: 24,
+            },
+        ]
+    },
+    {
+        name: "Denmark",
+        children: [
+            {
+                name: "Copenhagen",
+                population: 1.153615,
+                ranking: 25,
+            },
+        ]
+    },
+    {
+        name: "Bulgaria",
+        children: [
+            {
+                name: "Sofia",
+                population: 1.152556,
+                ranking: 26,
+            },
+        ]
+    },
+    {
+        name: "Ireland",
+        children: [
+            {
+                name: "Dublin",
+                population: 1.024027,
+                ranking: 35,
+            },
+        ]
+    },
+    {
+        name: "Belgium",
+        children: [
+            {
+                name: "Brussels",
+                population: 1.019022,
+                ranking: 36,
             },
         ]
     },
