8a3e1a8ccbf66c325a37444be13371d382975b1e
laurelisthompson <laurelis.thompson@gmail.com>
2021-02-05 18:54:41 -0500

    add south american cities

--- src/scripts/continents/south_america-cities.js
+++ src/scripts/continents/south_america-cities.js
@@ -4,12 +4,217 @@
 
 export const southAmerica = [
     {
-        name: "",
+        name: "Argentina",
         children: [
             {
-                name: "",
-                population: 0,
-                ranking: 0,
+                name: "Buenos Aires",
+                population: 13.0763,
+                ranking: 1,
+            },
+            {
+                name: "Cordoba",
+                population: 1.428214,
+                ranking: 21,
+            },
+            {
+                name: "Rosario",
+                population: 1.173533,
+                ranking: 29,
+            },
+        ]
+    },
+    {
+        name: "Brazil",
+        children: [
+            {
+                name: "São Paolo",
+                population: 10.021295,
+                ranking: 2,
+            },
+            {
+                name: "Rio de Janeiro",
+                population: 6.023699,
+                ranking: 5,
+            },
+            {
+                name: "Salvador",
+                population: 2.71184,
+                ranking: 8,
+            },
+            {
+                name: "Fortaleza",
+                population: 2.4,
+                ranking: 9,
+            },
+            {
+                name: "Belo Horizonte",
+                population: 2.373224,
+                ranking: 11,
+            },
+            {
+                name: "Brasilia",
+                population: 2.207718,
+                ranking: 13,
+            },
+            {
+                name: "Curitiba",
+                population: 1.718421,
+                ranking: 17,
+            },
+            {
+                name: "Manaus",
+                population: 1.59821,
+                ranking: 18,
+            },
+            {
+                name: "Recife",
+                population: 1.478098,
+                ranking: 20,
+            },
+            {
+                name: "Belem",
+                population: 1.407737,
+                ranking: 22,
+            },
+            {
+                name: "Porto Alegre",
+                population: 1.372741,
+                ranking: 26,
+            },
+            {
+                name: "Goiania",
+                population: 1.171195,
+                ranking: 30,
+            },
+            {
+                name: "Guarulhos",
+                population: 1.169577,
+                ranking: 31,
+            },
+            {
+                name: "Campinas",
+                population: 1.031554,
+                ranking: 32,
+            },
+            {
+                name: "Nova Iguacu",
+                population: 1.002118,
+                ranking: 33,
+            },
+        ]
+    },
+    {
+        name: "Peru",
+        children: [
+            {
+                name: "Lima",
+                population: 7.737002,
+                ranking: 3,
+            },
+        ]
+    },
+    {
+        name: "Colombia",
+        children: [
+            {
+                name: "Bogota",
+                population: 7.674366,
+                ranking: 4,
+            },
+            {
+                name: "Cali",
+                population: 2.392877,
+                ranking: 10,
+            },
+            {
+                name: "Medellin",
+                population: 1.999979,
+                ranking: 14,
+            },
+            {
+                name: "Barranquilla",
+                population: 1.380425,
+                ranking: 25,
+            },
+        ]
+    },
+    {
+        name: "Chile",
+        children: [
+            {
+                name: "Santiago",
+                population: 4.837295,
+                ranking: 6,
+            },
+        ]
+    },
+    {
+        name: "Venezuela",
+        children: [
+            {
+                name: "Caracas",
+                population: 3,
+                ranking: 7,
+            },
+            {
+                name: "Maracaibo",
+                population: 2.225,
+                ranking: 12,
+            },
+            {
+                name: "Maracay",
+                population: 1.754256,
+                ranking: 16,
+            },
+            {
+                name: "Valencia",
+                population: 1.385083,
+                ranking: 24,
+            },
+        ]
+    },
+    {
+        name: "Ecuador",
+        children: [
+            {
+                name: "Guayaquil",
+                population: 1.952029,
+                ranking: 15,
+            },
+            {
+                name: "Quito",
+                population: 1.399814,
+                ranking: 23,
+            },
+        ]
+    },
+    {
+        name: "Paraguay",
+        children: [
+            {
+                name: "Asuncion",
+                population: 1.4822,
+                ranking: 19,
+            },
+        ]
+    },
+    {
+        name: "Bolivia",
+        children: [
+            {
+                name: "Santa Cruz de la Sierra",
+                population: 1.364389,
+                ranking: 27,
+            },
+        ]
+    },
+    {
+        name: "Uruguay",
+        children: [
+            {
+                name: "Montevideo",
+                population: 1.270737,
+                ranking: 28,
             },
         ]
     },
