973b42de3c31b1e885efdf062a7a8fbb96ad8832
laurelisthompson <laurelis.thompson@gmail.com>
2021-02-05 18:08:25 -0500

    add north american cities

--- src/scripts/continents/north_america-cities.js
+++ src/scripts/continents/north_america-cities.js
@@ -4,12 +4,177 @@
 
 export const northAmerica = [
     {
-        name: "",
+        name: "Mexico",
         children: [
             {
-                name: "",
-                population: 0,
-                ranking: 0,
+                name: "Mexico City",
+                population: 12.294193,
+                ranking: 1,
+            },
+            {
+                name: "Iztapalapa",
+                population: 1.815786,
+                ranking: 9,
+            },
+            {
+                name: "Ecatepec de Morelos",
+                population: 1.655015,
+                ranking: 10,
+            },
+            {
+                name: "Guadalajara",
+                population: 1.495182,
+                ranking: 14,
+            },
+            {
+                name: "Puebla",
+                population: 1.434062,
+                ranking: 16,
+            },
+            {
+                name: "Juarez",
+                population: 1.321004,
+                ranking: 18,
+            },
+            {
+                name: "Tijuana",
+                population: 1.300983,
+                ranking: 19,
+            },
+            {
+                name: "Leon de Los Aldama",
+                population: 1.238962,
+                ranking: 21,
+            },
+            {
+                name: "Gustavo Adolfo Madero",
+                population: 1.185772,
+                ranking: 24,
+            },
+            {
+                name: "Zapopan",
+                population: 1.142483,
+                ranking: 25,
+            },
+            {
+                name: "Monterrey",
+                population: 1.135512,
+                ranking: 26,
+            },
+            {
+                name: "Ciudad Nezahualcoyotl",
+                population: 1.104585,
+                ranking: 27,
+            },
+        ]
+    },
+    {
+        name: "United States",
+        children: [
+            {
+                name: "New York City",
+                population: 2,
+                ranking: 8.175133,
+            },
+            {
+                name: "Los Angeles",
+                population: 3.971883,
+                ranking: 3,
+            },
+            {
+                name: "Chicago",
+                population: 2.720546,
+                ranking: 4,
+            },
+            {
+                name: "Houston",
+                population: 2.296224,
+                ranking: 6,
+            },
+            {
+                name: "Philadelphia",
+                population: 1.567442,
+                ranking: 12,
+            },
+            {
+                name: "Phoenix",
+                population: 1.563025,
+                ranking: 13,
+            },
+            {
+                name: "San Antonio",
+                population: 1.469845,
+                ranking: 15,
+            },
+            {
+                name: "San Diego",
+                population: 1.394928,
+                ranking: 17,
+            },
+            {
+                name: "Dallas",
+                population: 1.300092,
+                ranking: 20,
+            },
+            {
+                name: "San Jose",
+                population: 1.026908,
+                ranking: 28,
+            },
+        ]
+    },
+    {
+        name: "Canada",
+        children: [
+            {
+                name: "Toronto",
+                population: 2.6,
+                ranking: 5,
+            },
+            {
+                name: "Montreal",
+                population: 1.6,
+                ranking: 11,
+            },
+            {
+                name: "Calgary",
+                population: 1.019942,
+                ranking: 29,
+            },
+        ]
+    },
+    {
+        name: "Dominican Republic",
+        children: [
+            {
+                name: "Santo Domingo",
+                population: 2.201941,
+                ranking: 7,
+            },
+            {
+                name: "Santiago de los Caballeros",
+                population: 1.2,
+                ranking: 23,
+            },
+        ]
+    },
+    {
+        name: "Cuba",
+        children: [
+            {
+                name: "Havana",
+                population: 2.163824,
+                ranking: 8,
+            },
+        ]
+    },
+    {
+        name: "Haiti",
+        children: [
+            {
+                name: "Port-au-Prince",
+                population: 1.234742,
+                ranking: 22,
             },
         ]
     },
