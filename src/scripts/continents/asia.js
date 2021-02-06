7aa53deb12608700e537dd9b4b108e2f58cd5b3d
laurelisthompson <laurelis.thompson@gmail.com>
2021-02-05 20:59:06 -0500

    add population and area data

--- src/scripts/continents/asia.js
+++ src/scripts/continents/asia.js
@@ -5,8 +5,243 @@
 
 export const asia = [
     {
-        name: "Algeria",
-        population: 0,
-        area: 0,
+        name: "Afghanistan",
+        population: 39.835428,
+        area: 652230,
+    },
+    {
+        name: "Armenia",
+        population: 2.968127,
+        area: 29843,
+    },
+    {
+        name: "Azerbaijan",
+        population: 10.223342,
+        area: 86000,
+    },
+    {
+        name: "Bahrain",
+        population: 1.748296,
+        area: 780,
+    },
+    {
+        name: "Bangladesh",
+        population: 166.303498,
+        area: 147570,
+    },
+    {
+        name: "Bhutan",
+        population: .779898,
+        area: 38394,
+    },
+    {
+        name: "Brunei",
+        population: .441532,
+        area: 5765,
+    },
+    {
+        name: "Cambodia",
+        population: 16.946438,
+        area: 181035,
+    },
+    {
+        name: "China",
+        population: 1444.216107,
+        area: 9596961,
+    },
+    {
+        name: "Georgia",
+        population: 3.979765,
+        area: 69000,
+    },
+    {
+        name: "India",
+        population: 1393.409038,
+        area: 3287263,
+    },
+    {
+        name: "Indonesia",
+        population: 276.361783,
+        area: 1502029,
+    },
+    {
+        name: "Iran",
+        population: 85.028759,
+        area: 1648195,
+    },
+    {
+        name: "Iraq",
+        population: 41.179350,
+        area: 438317,
+    },
+    {
+        name: "Israel",
+        population: 8.789774,
+        area: 22072,
+    },
+    {
+        name: "Japan",
+        population: 126.050804,
+        area: 377930,
+    },
+    {
+        name: "Jordan",
+        population: 10.269021,
+        area: 89342,
+    },
+    {
+        name: "Kazakhstan",
+        population: 18.994962,
+        area: 2544900,
+    },
+    {
+        name: "Kuwait",
+        population: 4.328550,
+        area: 17818,
+    },
+    {
+        name: "Kyrgyzstan",
+        population: 6.628356,
+        area: 199951,
+    },
+    {
+        name: "Laos",
+        population: 7.379358,
+        area: 236800,
+    },
+    {
+        name: "Lebanon",
+        population: 6.769146,
+        area: 10452,
+    },
+    {
+        name: "Malaysia",
+        population: 32.776194,
+        area: 330803,
+    },
+    {
+        name: "Maldives",
+        population: .543617,
+        area: 300,
+    },
+    {
+        name: "Mongolia",
+        population: 3.329289,
+        area: 1564110,
+    },
+    {
+        name: "Myanmar",
+        population: 54.806012,
+        area: 676578,
+    },
+    {
+        name: "Nepal",
+        population: 29.674920,
+        area: 147181,
+    },
+    {
+        name: "North Korea",
+        population: 25.887041,
+        area: 120538,
+    },
+    {
+        name: "Oman",
+        population: 5.223375,
+        area: 309500,
+    },
+    {
+        name: "Pakistan",
+        population: 225.199937,
+        area: 881913,
+    },
+    {
+        name: "Palestine",
+        population: 5.222748,
+        area: 6020,
+    },
+    {
+        name: "Philippines",
+        population: 111.046913,
+        area: 300000,
+    },
+    {
+        name: "Qatar",
+        population: 2.930528,
+        area: 11586,
+    },
+    {
+        name: "Saudi Arabia",
+        population: 35.340682,
+        area: 2149690,
+    },
+    {
+        name: "Singapore",
+        population: 5.896686,
+        area: 725,
+    },
+    {
+        name: "South Korea",
+        population: 51.305186,
+        area: 100210,
+    },
+    {
+        name: "Sri Lanka",
+        population: 21.497310,
+        area: 65610,
+    },
+    {
+        name: "Syria",
+        population: 18.275702,
+        area: 185180,
+    },
+    {
+        name: "Taiwan",
+        population: 23.855010,
+        area: 36193,
+    },
+    {
+        name: "Tajikistan",
+        population: 9.749627,
+        area: 143100,
+    },
+    {
+        name: "Thailand",
+        population: 69.950850,
+        area: 513120,
+    },
+    {
+        name: "Timor Leste",
+        population: 1.343873,
+        area: 14874,
+    },
+    {
+        name: "Turkey",
+        population: 85.042738,
+        area: 759592,
+    },
+    {
+        name: "Turkmenistan",
+        population: 6.117924,
+        area: 488100,
+    },
+    {
+        name: "United Arab Emirates",
+        population: 9.991089,
+        area: 83600,
+    },
+    {
+        name: "Uzbekistan",
+        population: 33.935763,
+        area: 447400,
+    },
+    {
+        name: "Vietnam",
+        population: 98.168833,
+        area: 331212,
+    },
+    {
+        name: "Yemen",
+        population: 30.490640,
+        area: 527968,
     },
 ];
