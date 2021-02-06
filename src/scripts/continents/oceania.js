5826fd42a0303d53ebc954abb0d6ed75ad038a5d
laurelisthompson <laurelis.thompson@gmail.com>
2021-02-05 21:46:45 -0500

    add population and area data

--- src/scripts/continents/oceania.js
+++ src/scripts/continents/oceania.js
@@ -5,8 +5,73 @@
 
 export const oceania = [
     {
-        name: "",
-        population: 0,
-        area: 0,
+        name: "Australia",
+        population: 25.788215,
+        area: 7692024,
+    },
+    {
+        name: "Fiji",
+        population: .902906,
+        area: 18274,
+    },
+    {
+        name: "Kiribati",
+        population: .121392,
+        area: 811,
+    },
+    {
+        name: "Marshall Islands",
+        population: .059610,
+        area: 181,
+    },
+    {
+        name: "Micronesia",
+        population: .116254,
+        area: 702,
+    },
+    {
+        name: "Nauru",
+        population: .010876,
+        area: 21,
+    },
+    {
+        name: "New Zealand",
+        population: 4.860643,
+        area: 270467,
+    },
+    {
+        name: "Palau",
+        population: .018169,
+        area: 459,
+    },
+    {
+        name: "Papua New Guinea",
+        population: 9.119010,
+        area: 462840,
+    },
+    {
+        name: "Samoa",
+        population: .200149,
+        area: 2831,
+    },
+    {
+        name: "Solomon Islands",
+        population: .703996,
+        area: 28896,
+    },
+    {
+        name: "Tonga",
+        population: .106760,
+        area: 747,
+    },
+    {
+        name: "Tuvalu",
+        population: .011931,
+        area: 26,
+    },
+    {
+        name: "Vanuatu",
+        population: .314464,
+        area: 12189,
     },
 ];
--- src/scripts/continents/south_america.js
+++ src/scripts/continents/south_america.js
@@ -5,8 +5,63 @@
 
 export const southAmerica = [
     {
-        name: "",
-        population: 0,
-        area: 0,
+        name: "Argentina",
+        population: 45.605826,
+        area: 2780400,
+    },
+    {
+        name: "Bolivia",
+        population: 11.832940,
+        area: 1098581,
+    },
+    {
+        name: "Brazil",
+        population: 213.993437,
+        area: 8515799,
+    },
+    {
+        name: "Chile",
+        population: 19.212361,
+        area: 756102,
+    },
+    {
+        name: "Columbia",
+        population: 51.265844,
+        area: 1141748,
+    },
+    {
+        name: "Ecuador",
+        population: 17.888475,
+        area: 276841,
+    },
+    {
+        name: "Guyana",
+        population: .790326,
+        area: 214969,
+    },
+    {
+        name: "Paraguay",
+        population: 7.219638,
+        area: 406750,
+    },
+    {
+        name: "Peru",
+        population: 33.359418,
+        area: 1285216,
+    },
+    {
+        name: "Suriname",
+        population: .591800,
+        area: 163820,
+    },
+    {
+        name: "Uruguay",
+        population: 3.485151,
+        area: 176215,
+    },
+    {
+        name: "Venezuela",
+        population: 28.704954,
+        area: 916445,
     },
 ];
