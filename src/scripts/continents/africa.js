2184c6ff2cdd472d64a7edf05c7cc1d165f2570f
laurelisthompson <laurelis.thompson@gmail.com>
2021-02-05 20:40:06 -0500

    add population and area data

--- src/scripts/continents/africa.js
+++ src/scripts/continents/africa.js
@@ -5,8 +5,273 @@
 
 export const africa = [
     {
-        name: "",
-        population: 0,
-        area: 0,
+        name: "Algeria",
+        population: 4.4616624,
+        area: 2381741,
+    },
+    {
+        name: "Angola",
+        population: 3.3933610,
+        area: 1246700,
+    },
+    {
+        name: "Benin",
+        population: 12451040,
+        area: 112622,
+    },
+    {
+        name: "Botswana",
+        population: 2397241,
+        area: 582000,
+    },
+    {
+        name: "Burkina Faso",
+        population: 21497096,
+        area: 272967,
+    },
+    {
+        name: "Burundi",
+        population: 12255433,
+        area: 27834,
+    },
+    {
+        name: "Cameroon",
+        population: 27.224265,
+        area: 475442,
+    },
+    {
+        name: "Cabo Verde",
+        population: .561898,
+        area: 4033,
+    },
+    {
+        name: "Central African Republic",
+        population: 4.919981,
+        area: 622984,
+    },
+    {
+        name: "Chad",
+        population: 16.914985,
+        area: 1284000,
+    },
+    {
+        name: "Comoros",
+        population: .888451,
+        area: 1862,
+    },
+    {
+        name: "Djibouti",
+        population: 1.002187,
+        area: 23200,
+    },
+    {
+        name: "Democratic Republic of the Congo",
+        population: 92.377993,
+        area: 2344858,
+    },
+    {
+        name: "Egypt",
+        population: 104.258327,
+        area: 1002450,
+    },
+    {
+        name: "Equatorial Guinea",
+        population: 1.449896,
+        area: 28051,
+    },
+    {
+        name: "Eritrea",
+        population: 3.601467,
+        area: 117600,
+    },
+    {
+        name: "Ethiopia",
+        population: 117.876227,
+        area: 1104300,
+    },
+    {
+        name: "Gabon",
+        population: 2.278825,
+        area: 267668,
+    },
+    {
+        name: "Gambia",
+        population: 2.486945,
+        area: 10689,
+    },
+    {
+        name: "Ghana",
+        population: 31.732129,
+        area: 238533,
+    },
+    {
+        name: "Guinea",
+        population: 13.497244,
+        area: 245857,
+    },
+    {
+        name: "Guinea Bissau",
+        population: 2.015494,
+        area: 36125,
+    },
+    {
+        name: "Côte d'Ivoire",
+        population: 27.053629,
+        area: 322463,
+    },
+    {
+        name: "Kenya",
+        population: 54.985698,
+        area: 580367,
+    },
+    {
+        name: "Lesotho",
+        population: 2159079,
+        area: 30355,
+    },
+    {
+        name: "Liberia",
+        population: 5.180203,
+        area: 111369,
+    },
+    {
+        name: "Libya",
+        population: 6.958532,
+        area: 1759540,
+    },
+    {
+        name: "Madagascar",
+        population: 28.427328,
+        area: 587041,
+    },
+    {
+        name: "Malawi",
+        population: 19.647684,
+        area: 118484,
+    },
+    {
+        name: "Mali",
+        population: 20.855735,
+        area: 1240192,
+    },
+    {
+        name: "Mauritania",
+        population: 4.775119,
+        area: 1030700,
+    },
+    {
+        name: "Mauritius",
+        population: 1.273433,
+        area: 2040,
+    },
+    {
+        name: "Morocco",
+        population: 37.344795,
+        area: 446550,
+    },
+    {
+        name: "Mozambique",
+        population: 32.163047,
+        area: 801590,
+    },
+    {
+        name: "Namibia",
+        population: 2.587344,
+        area: 825615,
+    },
+    {
+        name: "Niger",
+        population: 25.130817,
+        area: 1267000,
+    },
+    {
+        name: "Nigeria",
+        population: 211.400708,
+        area: 923768,
+    },
+    {
+        name: "Republic of the Congo",
+        population: 5.657013,
+        area: 342000,
+    },
+    {
+        name: "Rwanda",
+        population: 13.276513,
+        area: 26338,
+    },
+    {
+        name: "São Tomé and Príncipe",
+        population: .223368,
+        area: 964,
+    },
+    {
+        name: "Senegal",
+        population: 17.196301,
+        area: 196772,
+    },
+    {
+        name: "Seychelles",
+        population: .98908,
+        area: 452,
+    },
+    {
+        name: "Sierra Leone",
+        population: 8.141343,
+        area: 71740,
+    },
+    {
+        name: "Somalia",
+        population: 16.359504,
+        area: 637657,
+    },
+    {
+        name: "South Africa",
+        population: 60.041994,
+        area: 1221037,
+    },
+    {
+        name: "South Sudan",
+        population: 11.381378,
+        area: 619745,
+    },
+    {
+        name: "Sudan",
+        population: 44.909353,
+        area: 1886068,
+    },
+    {
+        name: "Swaziland",
+        population: 1.172362,
+        area: 17364,
+    },
+    {
+        name: "Tanzania",
+        population: 61.498437,
+        area: 945087,
+    },
+    {
+        name: "Togo",
+        population: 8.478250,
+        area: 56785,
+    },
+    {
+        name: "Tunisia",
+        population: 11.935766,
+        area: 163610,
+    },
+    {
+        name: "Uganda",
+        population: 47.123531,
+        area: 241550,
+    },
+    {
+        name: "Zambia",
+        population: 18.920651,
+        area: 752612,
+    },
+    {
+        name: "Zimbabwe",
+        population: 15.092171,
+        area: 390757,
     },
 ];
