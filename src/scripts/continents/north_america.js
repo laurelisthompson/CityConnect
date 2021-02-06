708c83112ad799d826ff16b0fe9b7f14302d01f6
laurelisthompson <laurelis.thompson@gmail.com>
2021-02-05 21:32:40 -0500

    add population and area data

--- src/scripts/continents/north_america.js
+++ src/scripts/continents/north_america.js
@@ -5,8 +5,108 @@
 
 export const northAmerica = [
     {
-        name: "",
-        population: 0,
-        area: 0,
+        name: "Antigua and Barbuda",
+        population: .098731,
+        area: 442,
+    },
+    {
+        name: "Bahamas",
+        population: .396913,
+        area: 13943,
+    },
+    {
+        name: "Barbados",
+        population: .287711,
+        area: 430,
+    },
+    {
+        name: "Belize",
+        population: .404914,
+        area: 22966,
+    },
+    {
+        name: "Canada",
+        population: 38.067903,
+        area: 9984670,
+    },
+    {
+        name: "Costa Rica",
+        population: 5.139052,
+        area: 51100,
+    },
+    {
+        name: "Cuba",
+        population: 11.317505,
+        area: 109886,
+    },
+    {
+        name: "El Salvador",
+        population: 6.518499,
+        area: 21041,
+    },
+    {
+        name: "Grenada",
+        population: .113021,
+        area: 344,
+    },
+    {
+        name: "Guatemala",
+        population: 18.249860,
+        area: 108889,
+    },
+    {
+        name: "Haiti",
+        population: 11.541685,
+        area: 27750,
+    },
+    {
+        name: "Honduras",
+        population: 10.062991,
+        area: 112492,
+    },
+    {
+        name: "Jamaica",
+        population: 2.972463,
+        area: 10991,
+    },
+    {
+        name: "Mexico",
+        population: 130.262216,
+        area: 1964375,
+    },
+    {
+        name: "Nicaragua",
+        population: 6.702385,
+        area: 130373,
+    },
+    {
+        name: "Panama",
+        population: 4.381579,
+        area: 75417,
+    },
+    {
+        name: "Saint Kitts and Nevis",
+        population: .053544,
+        area: 261,
+    },
+    {
+        name: "Saint Lucia",
+        population: .184400,
+        area: 539,
+    },
+    {
+        name: "Saint Vincent and The Grenadines",
+        population: .111263,
+        area: 389,
+    },
+    {
+        name: "Trinidad and Tobago",
+        population: 1.403375,
+        area: 5130,
+    },
+    {
+        name: "United States",
+        population: 332.915073,
+        area: 9629091,
     },
 ];
